b3b2e50df239cc60c7bd7f3321f3926b66b804d0
Christopher Eckardt <christopher.eckardt@visual-meta.com>
2018-07-10 15:59:36 +0200

    add some more

--- src/main/java/crisscrosscrass/ReportWindow.java
+++ src/main/java/crisscrosscrass/ReportWindow.java
@@ -15,51 +15,7 @@ import java.net.URL;
 public class ReportWindow {
 
     public void MyReportWindow() throws IOException {
-        /*
-        window = new Stage();
-        window.setTitle("Frontend Report");
-        window.setWidth(500);
-        window.setHeight(450);
-        Text text = new Text();
-        String myText = "";
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File("temp//report.txt")))) {
-
-            String line;
-            while ((line = reader.readLine()) != null)
-                //System.out.println(line);
-                myText += line+"\n";
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        text.setText(myText);
-        String location = System.getProperty("user.dir");
-        location = location.replace("\\","//");
-        location += "//temp//";
-
-        Image image = new Image("file:///"+location+"screenshot1.png");
-        ImageView iv = new ImageView();
-        iv.setImage(image);
-        iv.setPreserveRatio(true);
-        iv.setFitWidth(300);
-        iv.setFitWidth(300);
-
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(text,iv);
-        Scene scene = new Scene(vbox);
-        window.setScene(scene);
-        window.show();
-
-
-
-        String location = System.getProperty("user.dir");
-        location = location.replace("\\","//");
-        location += "//temp//";
-        Image image = new Image("file:///"+location+"screenshot1.png");
-        ImageViewReport.setImage(image);
-        ImageViewReport.setPreserveRatio(true);
-        */
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ReportInterface.fxml"));
         Parent root = (Parent) fxmlLoader.load();
