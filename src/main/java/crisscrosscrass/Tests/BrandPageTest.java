d2f5d6174f44fd5061654f351cdff18abaa92157
Christopher Eckardt <christopher.eckardt@visual-meta.com>
2018-08-24 10:56:14 +0200

    added new testcase

--- src/main/java/crisscrosscrass/Tests/BrandPageTest.java
+++ src/main/java/crisscrosscrass/Tests/BrandPageTest.java
@@ -6,9 +6,11 @@ import crisscrosscrass.Tasks.ScreenshotViaWebDriver;
 import javafx.application.Platform;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
@@ -31,10 +33,46 @@ public class BrandPageTest {
                 webDriver.navigate().to(inputBrandPageOverview.getText().trim());
                 WebDriverWait wait = new WebDriverWait(webDriver, 10);
                 try{
-                    boolean isAvailable = webDriver.findElementByXPath(Homepage.getProperty("brandpage.links")) != null;
-                    List<WebElement> quickMenuLetters = webDriver.findElementsByXPath(Homepage.getProperty("brandpage.links"));
-                    for (WebElement quickMenuItem : quickMenuLetters){
-                        System.out.println(quickMenuItem.getText());
+                    boolean isAvailable = webDriver.findElementByXPath(Homepage.getProperty("brandpage.quicklinks")) != null;
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Homepage.getProperty("brandpage.quicklinks"))));
+                    List<WebElement> quickMenuLetters = webDriver.findElementsByXPath(Homepage.getProperty("brandpage.quicklinks"));
+                    ArrayList allCollectedLinks = new ArrayList();
+
+                    for (WebElement FillIns : quickMenuLetters){
+                        allCollectedLinks.add(FillIns.getAttribute("href").trim().toLowerCase());
+                    }
+
+                    for (int i = 0 ; i < allCollectedLinks.size() ; i++){
+                        ((JavascriptExecutor)webDriver).executeScript("window.open()");
+                        tabs = new ArrayList<>(webDriver.getWindowHandles());
+                        webDriver.switchTo().window(tabs.get(1)); //switches to new tab
+                        webDriver.get(allCollectedLinks.get(i).toString());
+                        report.writeToFile("Missing Logos on page " + webDriver.getCurrentUrl() + " :");
+
+                        if(webDriver.findElements(By.xpath(Homepage.getProperty("brandpage.boxwrapper.top"))).size() > 0){
+                            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Homepage.getProperty("brandpage.boxwrapper.top"))));
+                            List<WebElement> boxWrapperTop = webDriver.findElementsByXPath(Homepage.getProperty("brandpage.boxwrapper.top"));
+                            for (WebElement quickMenuItem : boxWrapperTop){
+                                report.writeToFile(quickMenuItem.getText());
+                            }
+                        }
+                        if(webDriver.findElements(By.xpath(Homepage.getProperty("brandpage.boxwrapper.bottom"))).size() > 0){
+                            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Homepage.getProperty("brandpage.boxwrapper.bottom"))));
+                            List<WebElement> boxWrapperBottom = webDriver.findElementsByXPath(Homepage.getProperty("brandpage.boxwrapper.bottom"));
+                            for (WebElement quickMenuItem : boxWrapperBottom){
+                                report.writeToFile(quickMenuItem.getText());
+                            }
+                        }
+                        if(webDriver.findElements(By.xpath(Homepage.getProperty("brandpage.brandbox"))).size() > 0){
+                            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Homepage.getProperty("brandpage.brandbox"))));
+                            List<WebElement> brandboxes = webDriver.findElementsByXPath(Homepage.getProperty("brandpage.brandbox"));
+                            for (WebElement quickMenuItem : brandboxes){
+                                report.writeToFile(quickMenuItem.getText());
+                            }
+                        }
+
+                        webDriver.switchTo().window(tabs.get(1)).close();
+                        webDriver.switchTo().window(tabs.get(0));
                     }
 
 
